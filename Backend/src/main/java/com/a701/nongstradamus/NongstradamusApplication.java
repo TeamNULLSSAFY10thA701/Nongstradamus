bab232c0020ee24ba385066f83e06b259e1c6128
반주현 <qkswngus@gmail.com>
2024-03-05 14:42:59 +0900

    Fix: 패키지명 수정

--- Backend/src/main/java/com/a701/nongstradamus/NongstradamusApplication.java
+++ Backend/src/main/java/com/a701/nongstradamus/NongstradamusApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.a701.nongstradamus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
