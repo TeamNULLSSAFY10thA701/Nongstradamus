2633be0b6730abf2a2e685df510aa6d00c5ab6a7
김시은 <sieun043@gamil.com>
2024-03-06 16:23:17 +0900

    feat(data): Update structure

--- Backend/src/main/java/com/a701/nongstradamus/data/service/WholeMarketDataService.java
+++ Backend/src/main/java/com/a701/nongstradamus/data/service/WholeMarketDataService.java
@@ -2,4 +2,6 @@ package com.a701.nongstradamus.data.service;
 
 public interface WholeMarketDataService {
 
+    void updateWholeMarketData();
+
 }
--- Backend/src/main/java/com/a701/nongstradamus/data/service/WholeMarketDataServiceImpl.java
+++ Backend/src/main/java/com/a701/nongstradamus/data/service/WholeMarketDataServiceImpl.java
@@ -1,12 +1,25 @@
 package com.a701.nongstradamus.data.service;
 
+import com.a701.nongstradamus.data.repository.ProductRepository;
+import com.a701.nongstradamus.data.repository.WholeMarketRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 @EnableScheduling
 public class WholeMarketDataServiceImpl implements WholeMarketDataService{
+
+    private final WholeMarketRepository wholeMarketRepository;
+
+    private final ProductRepository productRepository;
+
+    @Override
+    @Scheduled(cron = "0 0 0 * * *")
+    public void updateWholeMarketData() {
+
+    }
 
 }
