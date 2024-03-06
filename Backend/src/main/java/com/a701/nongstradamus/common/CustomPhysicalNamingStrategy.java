29555142c157894c44e09d58895e9093573b8759
김한중 <hjhzkc@gmail.com>
2024-03-06 13:35:58 +0900

    feat: Implement CustomPhysicalNamingStrategy

---
+++ Backend/src/main/java/com/a701/nongstradamus/common/CustomPhysicalNamingStrategy.java
@@ -0,0 +1,34 @@
+package com.a701.nongstradamus.common;
+
+import org.hibernate.boot.model.naming.Identifier;
+import org.hibernate.boot.model.naming.PhysicalNamingStrategy;
+import org.hibernate.engine.jdbc.env.spi.JdbcEnvironment;
+
+public class CustomPhysicalNamingStrategy implements PhysicalNamingStrategy {
+
+    @Override
+    public Identifier toPhysicalCatalogName(Identifier identifier, JdbcEnvironment jdbcEnvironment) {
+        return identifier;
+    }
+
+    @Override
+    public Identifier toPhysicalSchemaName(Identifier identifier, JdbcEnvironment jdbcEnvironment) {
+        return identifier;
+    }
+
+    @Override
+    public Identifier toPhysicalTableName(Identifier identifier, JdbcEnvironment jdbcEnvironment) {
+        return identifier;
+    }
+
+    @Override
+    public Identifier toPhysicalSequenceName(Identifier identifier,
+                                             JdbcEnvironment jdbcEnvironment) {
+        return identifier;
+    }
+
+    @Override
+    public Identifier toPhysicalColumnName(Identifier identifier, JdbcEnvironment jdbcEnvironment) {
+        return identifier;
+    }
+}
