0eb3067461f86913fae5622227cc3d569cde6121
김한중 <hjhzkc@gmail.com>
2024-03-07 16:36:52 +0900

    feat: :wrench:Add Scheduler to GobalOilService & DomesticOilService

--- Backend/src/main/java/com/a701/nongstradamus/data/service/DomesticOilService.java
+++ Backend/src/main/java/com/a701/nongstradamus/data/service/DomesticOilService.java
@@ -1,22 +1,47 @@
 package com.a701.nongstradamus.data.service;
 
-import com.a701.nongstradamus.data.domain.DomesticOilEntity;
-import com.a701.nongstradamus.data.domain.DomesticOilDto;
+import com.a701.nongstradamus.data.entity.DomesticOilEntity;
+import com.a701.nongstradamus.data.dto.DomesticOilDto;
+import com.a701.nongstradamus.data.mapper.DomesticOilMapper;
 import com.a701.nongstradamus.data.repository.DomesticOilRepository;
 import lombok.AllArgsConstructor;
-import org.modelmapper.ModelMapper;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cglib.core.Local;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-@AllArgsConstructor
 public class DomesticOilService {
 
-    private final ModelMapper modelMapper;
-    private final DomesticOilRepository repository;
+    @Value("${key.domesticOil.url}")
+    private String callBackUrl;
+    @Value("${key.domesticOil.servicekey}")
+    private String serviceKey;
+    private String dataType = "json";
 
+    private static JSONArray resultObjects;
+    private static LocalDate today;
+
+    private final DomesticOilRepository domesticOilRepository;
+
+    public DomesticOilService(DomesticOilRepository domesticOilRepository) {
+        this.domesticOilRepository = domesticOilRepository;
+    }
 
     public DomesticOilDto getDomesticOilByDate(DomesticOilEntity domesticOil) {
 
@@ -25,17 +50,18 @@ public class DomesticOilService {
 
     public List<DomesticOilDto> getAllDomesticOil() {
 
-        List<DomesticOilEntity> lst = repository.findAll();
-        List<DomesticOilDto> result =
-                lst.stream().map(p -> modelMapper.map(p, DomesticOilDto.class)).collect(Collectors.toList());
+        List<DomesticOilEntity> lst = domesticOilRepository.findAll();
+        List<DomesticOilDto> result = lst.stream()
+                .map(DomesticOilMapper.INSTANCE::fromEntityToDto) // 수정된 부분
+                .collect(Collectors.toList());
         return result;
     }
 
     public DomesticOilDto createDomesticOil(DomesticOilDto dto) {
 
-        DomesticOilEntity entity = modelMapper.map(dto, DomesticOilEntity.class);
+        DomesticOilEntity entity = DomesticOilMapper.INSTANCE.fromDtoToEntity(dto);
 
-        repository.save(entity);
+        domesticOilRepository.save(entity);
 
         return dto;
     }
@@ -48,5 +74,111 @@ public class DomesticOilService {
     public DomesticOilDto deleteDomesticOilByDate() {
 
         return null;
+    }
+
+
+    @Scheduled(cron="0/5 * * * * ?") // 매일 12시
+    public ResponseEntity<String> callForecastApi(
+//            @RequestParam(value="base_time") String baseTime,
+//            @RequestParam(value="base_date") String baseDate,
+//            @RequestParam(value="beach_num") String beachNum
+    ){
+        HttpURLConnection urlConnection = null;
+        InputStream stream = null;
+        String result = null;
+        resultObjects = null;
+
+        String urlStr = callBackUrl +
+                "?out=" + dataType +
+                "&code=" + serviceKey;
+        try {
+            URL url = new URL(urlStr);
+
+            urlConnection = (HttpURLConnection) url.openConnection();
+            stream = getNetworkConnection(urlConnection);
+//            result = readStreamToString(stream);
+
+//////////////////////////////////////////////////////////////////////////////
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+            StringBuilder stringBuilder = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+//            String jsonString = stringBuilder.toString();
+//
+//            // Parse JSON string using org.json.JSONObject
+//            JSONObject jsonObject = new JSONObject(jsonString);
+//            JSONObject resultObject = jsonObject.getJSONObject("RESULT")
+//                                                .getJSONArray("OIL").getJSONObject(1);
+//
+//            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+//            LocalDate date = LocalDate.parse(resultObject.getString("TRADE_DT"), formatter);
+//
+//            Double avgPrice = resultObject.getDouble("PRICE");
+            String jsonString = stringBuilder.toString();
+
+            // Parse JSON string using org.json.JSONObject
+            JSONObject jsonObject = new JSONObject(jsonString);
+            JSONArray resultObjects = jsonObject.getJSONObject("RESULT").getJSONArray("OIL");
+
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+            today = LocalDate.now();
+
+            DomesticOilDto domesticOilDto = new DomesticOilDto(0,
+                    resultObjects.getJSONObject(0).getDouble("PRICE"),
+                    resultObjects.getJSONObject(1).getDouble("PRICE"),
+                    resultObjects.getJSONObject(2).getDouble("PRICE"),
+                    today
+            );
+            System.out.println(createDomesticOil(domesticOilDto));
+
+//            {"TRADE_DT":"20240305","PRODCD":"B027","PRICE":"1639.41","DIFF":"+0.46","PRODNM":"?��발유"}
+
+            // Extract values and populate your DomesticOilDto
+
+//////////////////////////////////////////////////////////////////////////////
+
+            if (stream != null) stream.close();
+
+        } catch(IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+        }
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    /* URLConnection 을 전달받아 연결정보 설정 후 연결, 연결 후 수신한 InputStream 반환 */
+    private InputStream getNetworkConnection(HttpURLConnection urlConnection) throws IOException {
+        urlConnection.setConnectTimeout(6000);
+        urlConnection.setReadTimeout(6000);
+        urlConnection.setRequestMethod("GET");
+        urlConnection.setDoInput(true);
+
+        if(urlConnection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+            throw new IOException("HTTP error code : " + urlConnection.getResponseCode());
+        }
+
+        return urlConnection.getInputStream();
+    }
+
+    /* InputStream을 전달받아 문자열로 변환 후 반환 */
+    private String readStreamToString(InputStream stream) throws IOException {
+        StringBuilder result = new StringBuilder();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(stream, "UTF-8"));
+
+        String readLine;
+        while((readLine = br.readLine()) != null) {
+            result.append(readLine);
+        }
+
+        br.close();
+
+        return result.toString();
     }
 }
---
+++ Backend/src/main/java/com/a701/nongstradamus/data/service/GlobalOilService.java
@@ -0,0 +1,180 @@
+package com.a701.nongstradamus.data.service;
+
+import com.a701.nongstradamus.data.entity.GlobalOilEntity;
+import com.a701.nongstradamus.data.dto.GlobalOilDto;
+import com.a701.nongstradamus.data.mapper.GlobalOilMapper;
+import com.a701.nongstradamus.data.repository.GlobalOilRepository;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Service
+public class GlobalOilService {
+
+    @Value("${key.globalOil.url}")
+    private String callBackUrl;
+    @Value("${key.globalOil.servicekey}")
+    private String serviceKey;
+    private final GlobalOilRepository globalOilRepository;
+    private String dataType = "json";
+    private Double sumValue;
+    private JsonNode targetNode;
+    private Map<String, JsonNode> nodeMap;
+
+    public GlobalOilService(GlobalOilRepository globalOilRepository) {
+        this.globalOilRepository = globalOilRepository;
+    }
+
+    public GlobalOilDto getGlobalOilByDate(GlobalOilEntity domesticOil) {
+
+        return null;
+    }
+
+    public List<GlobalOilDto> getAllGlobalOil() {
+
+        List<GlobalOilEntity> lst = globalOilRepository.findAll();
+        List<GlobalOilDto> result = lst.stream()
+                .map(GlobalOilMapper.INSTANCE::fromEntityToDto)
+                .collect(Collectors.toList());
+        return result;
+    }
+
+    public GlobalOilDto createGlobalOil(GlobalOilDto dto) {
+
+        GlobalOilEntity entity = GlobalOilMapper.INSTANCE.fromDtoToEntity(dto);
+
+        globalOilRepository.save(entity);
+
+        return dto;
+    }
+
+    public GlobalOilDto updateGlobalOilByDate(GlobalOilDto dto) {
+
+        return null;
+    }
+
+    public GlobalOilDto deleteGlobalOilByDate() {
+
+        return null;
+    }
+     @Scheduled(cron="0 0 12 * * ?") // 매일 12시
+    public ResponseEntity<String> callForecastApi(
+//            @RequestParam(value="base_time") String baseTime,
+//            @RequestParam(value="base_date") String baseDate,
+//            @RequestParam(value="beach_num") String beachNum
+    ){
+        HttpURLConnection urlConnection = null;
+        InputStream stream = null;
+        String result = null;
+        // 7일 전의 유가를 가져옴
+        String today = LocalDate.now().minusDays(7).toString();
+
+        // 요청 url 주소
+        String urlStr = callBackUrl + "?frequency=daily&data[0]=value&start="
+                + today + "&end=" + today
+                + "&sort[0][column]=period&sort[0][direction]=desc&offset=0&length=5000&api_key="
+                + serviceKey;
+        try {
+            URL url = new URL(urlStr);
+
+            urlConnection = (HttpURLConnection) url.openConnection();
+            stream = getNetworkConnection(urlConnection);
+//            result = readStreamToString(stream);
+
+//////////////////////////////////////////////////////////////////////////////
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+            StringBuilder stringBuilder = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+            String jsonString = stringBuilder.toString();
+
+            // ObjectMapper 생성
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            // JSON을 Jackson의 JsonNode로 파싱
+            JsonNode rootNode = objectMapper.readTree(jsonString);
+
+            // 루트 노드 검색
+            targetNode = null;
+            targetNode = rootNode.get("response").get("data");
+
+            // hashmap으로 저장
+            nodeMap = new HashMap<>();
+            for (JsonNode element : targetNode) {
+                nodeMap.put(element.get("series").asText(), element);
+            }
+
+            if (stream != null) stream.close();
+
+        } catch(IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+        }
+
+        if (nodeMap != null) {  // 데이터가 없으면 저장하지 않음
+            GlobalOilDto dto = new GlobalOilDto(0,
+                    nodeMap.get("RWTC").get("value").asDouble(),
+                    nodeMap.get("RBRTE").get("value").asDouble(),
+                    nodeMap.get("EER_EPD2DXL0_PF4_RGC_DPG").get("value").asDouble(),
+                    nodeMap.get("EER_EPMRU_PF4_RGC_DPG").get("value").asDouble(),
+                    LocalDate.now()
+            );
+            System.out.println(createGlobalOil(dto));
+//            createGlobalOil(dto);
+        }
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    /* URLConnection 을 전달받아 연결정보 설정 후 연결, 연결 후 수신한 InputStream 반환 */
+    private InputStream getNetworkConnection(HttpURLConnection urlConnection) throws IOException {
+        urlConnection.setConnectTimeout(3000);
+        urlConnection.setReadTimeout(3000);
+        urlConnection.setRequestMethod("GET");
+        urlConnection.setDoInput(true);
+
+        if(urlConnection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+            throw new IOException("HTTP error code : " + urlConnection.getResponseCode());
+        }
+
+        return urlConnection.getInputStream();
+    }
+
+    /* InputStream을 전달받아 문자열로 변환 후 반환 */
+    private String readStreamToString(InputStream stream) throws IOException {
+        StringBuilder result = new StringBuilder();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(stream, "UTF-8"));
+
+        String readLine;
+        while((readLine = br.readLine()) != null) {
+            result.append(readLine);
+        }
+
+        br.close();
+
+        return result.toString();
+    }
+}
