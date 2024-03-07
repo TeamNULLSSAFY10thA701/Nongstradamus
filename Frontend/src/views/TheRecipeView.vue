f73ca6180f9a6e3ec7cddb9cd0189d1b19d4c488
jangtaesoo <use02042@naver.com>
2024-03-07 16:30:21 +0900

    :sparkles: [feat] 오늘의 레시피 ui 작성

--- Frontend/src/views/TheRecipeView.vue
+++ Frontend/src/views/TheRecipeView.vue
@@ -1,51 +1,56 @@
 <template>
-  <Header></Header>
-  <div class="flex items-center justify-center mt-12">
-    <a
-      class="block max-w-sm p-6 bg-sky-300 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
-    >
-      <h5 class="mb-2 text-2xl font-bold tracking-tight text-white">
-        가격이 가장 많이 떨어졌어요!
-      </h5>
-      <div class="grid grid-cols-5 gap-4">
-        <div
-          class="col-span-3 text-center font-bold text-3xl text-white mt-6 mr-4"
-        >
-          품목
-        </div>
-        <div
-          class="col-span-2 text-center font-bold text-xl rounded-lg bg-white border mt-8 mr-2"
-        >
-          가격
+  <div class="max-w-md mx-auto">
+    <!-- startheader -->
+    <div class="grid grid-cols-3 gap-4 ml-4 mr-4 mt-8">
+      <div>
+        <img src="@/assets/full_logo1.png" />
+      </div>
+      <div class="col-span-2 flex items-center justify-center text-3xl">
+        농스트라다무스의 밥도둑
+      </div>
+    </div>
+    <!-- end header -->
+
+    <!-- start body -->
+    <div v-for="data in foodDatas" :key="data" class="m-12">
+      <div
+        class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+      >
+        <div class="flex flex-col items-center py-10">
+          <img
+            class="w-24 h-24 mb-3 rounded-full shadow-lg"
+            :src="`../../src/assets/food${data}.png`"
+            alt="Bonnie image"
+          />
+          <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">
+            Bonnie Green
+          </h5>
+          <span class="text-sm text-gray-500 dark:text-gray-400"
+            >Visual Designer</span
+          >
+          <div class="flex mt-4 md:mt-6">
+            <a
+              href="#"
+              class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >Add friend</a
+            >
+            <a
+              href="#"
+              class="py-2 px-4 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+              >Message</a
+            >
+          </div>
         </div>
       </div>
-    </a>
-  </div>
-  <div class="flex items-center justify-center mt-8">
-    <a
-      class="block max-w-sm p-6 bg-indigo-400 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
-    >
-      <h5 class="custom mb-2 font-bold tracking-tight text-white">
-        가격이 가장 많이 떨어질거에요!
-      </h5>
-      <div class="grid grid-cols-5 gap-4">
-        <div
-          class="col-span-3 text-center font-bold text-3xl text-white mt-6 mr-4"
-        >
-          품목
-        </div>
-        <div
-          class="col-span-2 text-center font-bold text-xl rounded-lg bg-white border mt-8 mr-2"
-        >
-          가격
-        </div>
-      </div>
-    </a>
+    </div>
+    <!-- end body -->
   </div>
 </template>
 
 <script setup>
-import Header from "@/components/Header.vue";
+import { ref } from "vue";
+
+const foodDatas = [1, 2, 3, 4];
 </script>
 
 <style scoped></style>
