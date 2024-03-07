ca97a785bb240132f2df1ddb224dc4a5dd26a510
우혁 <dngur12kr@naver.com>
2024-03-07 13:21:23 +0900

    :zap: 요구사항 1차

--- Frontend/src/components/main/TheMain.vue
+++ Frontend/src/components/main/TheMain.vue
@@ -9,6 +9,7 @@
             </div>
 
         </div>
+        <!-- header -->
         <div class="flex items-center justify-center mt-12 ">
             <a
                 class="block max-w-sm p-6 bg-sky-300 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
@@ -24,6 +25,7 @@
                 </div>
             </a>
         </div>
+        <!-- 가격이 가장 많이 떨어진 품목 조회 -->
         <div class="flex items-center justify-center mt-8">
             <a
                 class="block max-w-sm p-6 bg-indigo-400 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
@@ -39,6 +41,7 @@
                 </div>
             </a>
         </div>
+        <!-- 가격이 가장 많이 떨어질 것 같은 품목 조회 -->
         <div class="flex items-center justify-center mt-8">
             <a href="#"
                 class="block max-w-sm p-6 bg-indigo-400 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
@@ -51,6 +54,11 @@
                         오늘의 추천 레시피</div>
                 </div>
             </a>
+        </div>
+        <!-- 오늘의 추천 레시피 이동 -->
+
+        <div>
+
         </div>
     </div>
 
