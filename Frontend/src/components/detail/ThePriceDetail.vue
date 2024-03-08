91ebf29f26aa7d1ac80ef70d3325ddfabdc36a4e
우혁 <dngur12kr@naver.com>
2024-03-08 09:42:21 +0900

    :zap: line graph 적용

--- Frontend/src/components/detail/ThePriceDetail.vue
+++ Frontend/src/components/detail/ThePriceDetail.vue
@@ -1,8 +1,31 @@
 <template>
-    <div>
-
+    <div class="max-w-md mx-auto">
+        <Line :data="chartData" :options="chartOptions" />
     </div>
+
 </template>
+
 <script setup>
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js'
+import { Line } from 'vue-chartjs'
+
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend)
+
+const chartData = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [
+        {
+            label: 'Data One',
+            backgroundColor: '#f87979',
+            data: [40, 39, 10, 40, 39, 80, 40]
+        }
+    ]
+}
+
+const chartOptions = {
+    responsive: true,
+    maintainAspectRatio: false
+}
 </script>
+
 <style scoped></style>
--- Frontend/src/views/ThePriceDetailView.vue
+++ Frontend/src/views/ThePriceDetailView.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <ThePriceDetail></ThePriceDetail>
+
     </div>
 </template>
 <script setup>
