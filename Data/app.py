6c68ae8e8f1691791be8a97613b64e1185681945
김시은 <sieun043@gamil.com>
2024-03-11 14:01:32 +0900

    :fire: feat(sql): connect to database

--- Data/app.py
+++ Data/app.py
@@ -1,12 +1,19 @@
 from flask import Flask
+from apscheduler.schedulers.background import BackgroundScheduler
+from database import db_connection
+from sqlalchemy.orm import Session
 
 app = Flask(__name__)
 
+def calculate_price() :
+  engin = db_connection().get_engin()
+  session = Session(engin)
 
-@app.route('/')
-def hello_world():  # put application's code here
-  return 'Hello World!'
 
+scheduler = BackgroundScheduler()
+scheduler.add_job(calculate_price, 'interval', seconds=10)
+scheduler.add_job(calculate_price, 'cron', hour=1)
+scheduler.start()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
   app.run()
