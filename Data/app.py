7513159a358539d66f9f8a1badbc6e1ef47edb5b
김시은 <sieun043@gamil.com>
2024-03-14 13:32:59 +0900

    :fire: feat(sql): Add weather

--- Data/app.py
+++ Data/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from apscheduler.schedulers.background import BackgroundScheduler
-from nongstradmus_database import connection, price_history_get_stmt, product_get_stmt, wholesale_market_get_stmt, trade_get_stmt, domestic_oil_get_stmt, global_oil_get_stmt
+from nongstradmus_database import connection, price_history_get_stmt, product_get_stmt, wholesale_market_get_stmt, trade_get_stmt, domestic_oil_get_stmt, global_oil_get_stmt, weather_get_stmt
 from sqlalchemy.orm import Session
 import datetime
 import pandas
@@ -20,6 +20,7 @@ def calculate_price() :
     price_history = pandas.read_sql(price_history_get_stmt(product[0], start_date, end_date), conn)
     wholesale_market = pandas.read_sql(wholesale_market_get_stmt(product[0], start_date, end_date), conn)
     trade = pandas.read_sql(trade_get_stmt(product[0], start_date, end_date), conn)
+    wheater = pandas.read_sql(weather_get_stmt(product[0], start_date, end_date), conn)
 
 
 
