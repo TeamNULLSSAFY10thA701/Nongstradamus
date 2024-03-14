1487bcf4ed55480f9251e5c8b23e74b20c65dae4
jangtaesoo <use02042@naver.com>
2024-03-14 17:55:49 +0900

    드롭다운 텍스트 변경

--- Frontend/src/components/recipe/TheRecipeBody.vue
+++ Frontend/src/components/recipe/TheRecipeBody.vue
@@ -1,44 +1,48 @@
 <!-- TheRecipeBody.vue -->
 
 <template>
-  <div class="searchbar">
- 
-  <form class="max-w-lg mx-auto">
-    <div class="flex">
+  <!-- 검색창 start -->
+  <div class="searchbar form-container">
+  <form class="max-w-lg mx-auto flex">
+    <div class="flex items-center w-full">
+      <div style="position:relative">
         <label for="search-dropdown" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"></label>
-        <button id="dropdown-button" data-dropdown-toggle="categories" class="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-s-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600" type="button">All categories <svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
-            </svg>
+        <button id="dropdown-button" @click="toggleVisibility" class="flex-shrink-0 inline-flex items-center justify-center py-2.5 px-4 text-sm font-normal text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-s-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600" type="button">{{ dropdownText }} <svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+        </svg>
         </button>
-        <div id="categories" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-            <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdown-button">
+        <div v-if="isVisible" id="categories" class="z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700" style="position: absolute; top: 100%;">
+          <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdown-button">
             <li>
-                <button type="button" class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">제목</button>
+              <button type="button" @click="dropdownTextChange('제목')" class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">제목</button>
             </li>
             <li>
-                <button type="button" class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">제작자</button>
+              <button type="button" @click="dropdownTextChange('제작자')" class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">제작자</button>
             </li>
             <li>
-                <button type="button" class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">성분</button>
+              <button type="button" @click="dropdownTextChange('성분')" class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">성분</button>
             </li>
             <li>
-                <button type="button" class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">조리방법</button>
+              <button type="button" @click="dropdownTextChange('조리방법')" class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">조리방법</button>
             </li>
-            </ul>
+          </ul>
         </div>
-        <div class="relative w-full">
-            <input v-model.lazy="query" type="search" id="search-dropdown" class="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-s-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500" placeholder="Search" required />
-            <button @click="filterRecipes($event)" type="submit" class="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-e-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-                </svg>
-                <span class="sr-only">Search</span>
-            </button>
-        </div>
+      </div>
+      <div class="relative flex-1">
+        <input v-model.lazy="query" type="search" id="search-dropdown" class="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-s-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500" placeholder="Search" required />
+        <button @click="filterRecipes($event)" type="submit" class="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-e-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+          <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+          </svg>
+          <span class="sr-only">Search</span>
+        </button>
+      </div>
     </div>
   </form>
-
 </div>
+<p class="text-center m-3">'{{ selectedquery }}'로 검색한 결과</p>
+  <!-- 검색창 end-->
+
     <div class="recipe-list">
       <div v-for="data in filteredRecipe" :key="data.id" class="m-12 photo">
         <!-- 각 레시피 카드 -->
@@ -227,12 +231,24 @@
   const store = useRecipeStore();
   const recipes = ref(store.recipes)
   const query = ref('') // 사용자의 검색어(동적 바인딩)
+  const selectedquery = ref('')
   const filteredRecipe = ref([])
+  const isVisible = ref(false)
 
   onMounted(() => {
     filteredRecipe.value = store.recipes
-    console.log(recipes.value)
 })
+
+  // 분류 드롭다운 버튼의 보이기, 안보이기 설정
+  const toggleVisibility = () => {
+    isVisible.value = !isVisible.value
+  }
+
+  // 드롭다운 버튼 클릭 시 버튼의 텍스트 변경
+  const dropdownText = ref('분류')
+  const dropdownTextChange = (name) => {
+    dropdownText.value = name
+  }
 
   // 각 레시피의 정보들을 위한 변수
   const goToModal = (data, type) => {
@@ -245,24 +261,21 @@
     event.preventDefault();
     filteredRecipe.value = []
     if (!query.value || query.value.trim() === ''){
-      console.log(store.recipes)
       filteredRecipe.value = store.recipes;
     
       } 
     else {
       filteredRecipe.value = recipes.value.filter(recipe => {
-        console.log(recipe.성분)
         const ingredients = Object.keys(recipe['성분']);
         return ingredients.some(ingredient => ingredient.includes(query.value));
     })
     }
+    selectedquery.value = query.value
   }
-
-    
-
   </script>
   
   <style scoped>
-  /* 스타일링 */
+
+
   </style>
   
