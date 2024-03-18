5dd05bb1ec18e96171557a2bc1a780551946b14e
jangtaesoo <use02042@naver.com>
2024-03-18 13:12:12 +0900

    모달 조정 및 검색 기능 조정

--- Frontend/src/components/recipe/TheRecipeBody.vue
+++ Frontend/src/components/recipe/TheRecipeBody.vue
@@ -161,29 +161,7 @@
                       >
                         조리방법
                       </h3>
-                      <!-- 모달 닫기 버튼 -->
-                      <button
-                        type="button"
-                        class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                        data-modal-hide="modalEl2"
-                      >
-                        <svg
-                          class="w-3 h-3"
-                          aria-hidden="true"
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 14 14"
-                        >
-                          <path
-                            stroke="currentColor"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-                          />
-                        </svg>
-                        <span class="sr-only">Close modal</span>
-                      </button>
+                      
                     </div>
                     <!-- Modal body -->
                     <div class="p-4 md:p-5 space-y-4">
