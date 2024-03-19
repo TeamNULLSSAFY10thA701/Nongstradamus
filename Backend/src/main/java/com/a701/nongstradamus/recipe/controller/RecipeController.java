368736e368f548aafb3517421f7dcd4f6fb7ab1b
김시은 <sieun043@gamil.com>
2024-03-19 15:20:57 +0900

    :fire: feature(recipe): Add annotations

--- Backend/src/main/java/com/a701/nongstradamus/recipe/controller/RecipeController.java
+++ Backend/src/main/java/com/a701/nongstradamus/recipe/controller/RecipeController.java
@@ -1,6 +1,8 @@
 package com.a701.nongstradamus.recipe.controller;
 
+import com.a701.nongstradamus.recipe.service.RecipeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,5 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 @RequestMapping("/api/recipe")
 public class RecipeController {
+
+    private final RecipeService recipeService;
 
 }
--- Backend/src/main/java/com/a701/nongstradamus/recipe/service/RecipeServieImpl.java
+++ Backend/src/main/java/com/a701/nongstradamus/recipe/service/RecipeServieImpl.java
@@ -1,5 +1,10 @@
 package com.a701.nongstradamus.recipe.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class RecipeServieImpl implements RecipeService{
 
 }
