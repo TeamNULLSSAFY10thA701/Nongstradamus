47b6989094a76e7c381701e7d8be74a1d9099872
김시은 <sieun043@gamil.com>
2024-03-19 15:17:04 +0900

    :fire: feature(recipe): Add service and controller

---
+++ Backend/src/main/java/com/a701/nongstradamus/recipe/controller/RecipeController.java
@@ -0,0 +1,13 @@
+package com.a701.nongstradamus.recipe.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/api/recipe")
+public class RecipeController {
+
+}
