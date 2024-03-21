1458d429dbcf06247eba17eb531852a669263663
김평섭 <k1996914@naver.com>
2024-03-21 14:49:27 +0900

    common dto로 통합

--- Backend/src/main/java/com/a701/nongstradamus/advice/GlobalExceptionHandler.java
+++ Backend/src/main/java/com/a701/nongstradamus/advice/GlobalExceptionHandler.java
@@ -6,30 +6,30 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import com.a701.nongstradamus.util.ErrorResponse;
+import com.a701.nongstradamus.common.CommonDto;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     // EntityNotFoundException 처리(404)
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleEntityNotFoundException(EntityNotFoundException ex) {
+    public ResponseEntity<CommonDto> handleEntityNotFoundException(EntityNotFoundException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
-            .body(new ErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage()));
+            .body(new CommonDto(HttpStatus.NOT_FOUND.value(), ex.getMessage()));
     }
 
     // HttpMethodNotSupportedException 처리
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-    public ResponseEntity<ErrorResponse> handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException ex) {
+    public ResponseEntity<CommonDto> handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException ex) {
         return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED)
-            .body(new ErrorResponse(HttpStatus.NOT_IMPLEMENTED.value(), "지원하지 않는 메소드입니다."));
+            .body(new CommonDto(HttpStatus.NOT_IMPLEMENTED.value(), "지원하지 않는 메소드입니다."));
     }
 
     // NoResourceFoundException 처리
     @ExceptionHandler(org.springframework.web.servlet.resource.NoResourceFoundException.class)
-    public ResponseEntity<ErrorResponse> handleNoResourceFoundException(org.springframework.web.servlet.resource.NoResourceFoundException ex) {
+    public ResponseEntity<CommonDto> handleNoResourceFoundException(org.springframework.web.servlet.resource.NoResourceFoundException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
-            .body(new ErrorResponse(HttpStatus.NOT_IMPLEMENTED.value(), "지원하지 않는 주소입니다."));
+            .body(new CommonDto(HttpStatus.NOT_IMPLEMENTED.value(), "지원하지 않는 주소입니다."));
     }
 
 }
